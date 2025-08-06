a11b5fd90778f699c73d98a5ccd598870b4e0c85
KethanYang <yuyang.koh@redalphacyber.com>
2025-08-06 12:52:26 +0800

    updating main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("fireflys are neither fire nor flies")
+def add(a, b):
+	return a + b
+
+def test_add():
+	assert add(3, 5) == 1 #Failing cause math not good
+
+if __name__ == "__main__":
+	test_add()
